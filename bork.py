e96b41e296817e5cf6887f99bbb635d1bced7183
Ellen Marie Dash <me@duckie.co>
2019-06-24 19:26:37 -0400

    more work on the prototype version

--- bork.py
+++ bork.py
@@ -2,6 +2,7 @@
 
 import configparser
 from pathlib import Path
+import shutil
 import sys
 import zipapp
 
@@ -13,27 +14,24 @@ import twine
 DEFAULT_PYTHON_INTERPRETER = "/usr/bin/env python3"
 
 def print_help():
-    print("Usage: bork build SOURCE_DIR")
-    print("       bork clean SOURCE_DIR")
-    print("       bork release [--pypi] [--github]")
+    print("Usage: bork build")
+    print("       bork clean")
+    print("       bork release [pypi] [github]")
 
-def load_setup_cfg(source_dir):
-    setup_cfg_path = Path(source_dir, 'setup.cfg').resolve()
+def load_setup_cfg():
     setup_cfg = configparser.ConfigParser()
-    setup_cfg.read(setup_cfg_path)
+    setup_cfg.read('setup.cfg')
     return setup_cfg
 
-
 def build_pep517(args):
-    source_dir = args[0]
-    pep517.build.main(pep517.build.parser.parse_args(source_dir))
+    pep517.build.main(pep517.build.parser.parse_args('.', *args))
 
 def build_zipapp(args):
-    source_dir = args[0]
-    config = load_setup_cfg(source_dir)
+    config = load_setup_cfg()
     name = config['metadata']['name']
 
-    source = str(Path(source_dir, name))
+    # The code is assumed to be in ./<name>
+    source = str(Path(name))
 
     # TODO: Get version info -- this is more complex, since it can be in many places.
     #target = "dist/{}-{}.pyz".format(name, version)
@@ -57,31 +55,32 @@ def build_zipapp(args):
         raise Exception("Failed to build zipapp: {}".format(target))
 
 def build(args):
-    if len(args) < 1:
-        print_help()
-        exit(1)
-
     build_pep517(args)
     build_zipapp(args)
 
+def _try_delete(path):
+    if Path(path).is_dir():
+        shutil.rmtree(path)
+    elif Path(path).exists():
+        raise Exception("{} is not a directory".format(path))
+
 def clean(args):
-    if len(args) == 1:
-        source_dir = args[0]
-    else:
-        source_dir = '.'
+    config = load_setup_cfg()
+    name = config['metadata']['name']
 
     # TODO: Delete ./build ./dist ./<name>.egg-info
-    pass
+    _try_delete("./build")
+    _try_delete("./dist")
+    _try_delete("./{}.egg-info".format(name))
 
 def release(args):
-    pass
-
+    print("Would release to: {}".format(str(args)))
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    if len(argv) < 2 or "-h" in argv or "--help" in argv:
+    if len(argv) < 1 or "-h" in argv or "--help" in argv:
         print_help()
         exit(1)
 
