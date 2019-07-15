fe7e600d480d62bbc461799b7e4a68ee2333bd80
Ellen Marie Dash <me@duckie.co>
2019-07-15 12:24:20 -0400

    Add the ability to download PyPi packages.

--- bork/pypi.py
+++ bork/pypi.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+from urllib.request import urlopen
+
 from twine.cli import dispatch as twine_upload
 from .filesystem import find_files
+from .pypi_api import get_download_info
 
 
 def upload(*globs, dry_run=False):
@@ -9,3 +13,22 @@ def upload(*globs, dry_run=False):
         print('NOTE: Skipping PyPI upload step since this is a dry run.')
     else:
         twine_upload(["upload", *files])
+
+
+def download(package, release, file_pattern, directory):
+    base_url = 'https://pypi.org/simple'
+
+    asset_list = get_download_info(base_url, package, release, file_pattern)
+
+    directory = Path(directory)
+    directory.mkdir(parents=True, exist_ok=True)
+
+    for asset in asset_list:
+        name = asset['name']
+        url = asset['url']
+        path = directory / name
+
+        contents = urlopen(url).read()
+
+        path.write_bytes(contents)
+        print(str(path))
