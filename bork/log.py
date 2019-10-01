78edf23a173d034fc3b737e923705d8578983aca
Nicolas Braud-Santoni <nicolas@braud-santoni.eu>
2019-10-01 23:59:32 +0200

    bork.log.logger: Correctly resolve module names
    Thanks to Jamie for figuring out this bit of dark sourcery.
    You are the best snek witch  <3

--- bork/log.py
+++ bork/log.py
@@ -1,6 +1,28 @@
 import inspect
 import logging
+import sys
 from typing import Any, Callable, Optional, TypeVar
+
+
+def _get_module(context: inspect.FrameInfo):
+    """Find the module name for a given FrameInfo.
+
+    Raises KeyError if the module isn't loaded.
+
+    Pilfered from Jamie Bliss' equivalent in pursuedpybear:
+      https://github.com/ppb/pursuedpybear/blob/master/ppb/utils.py
+    """
+    if context.filename not in _get_module.index:
+        _get_module.index = {
+            mod.__file__: mod.__name__
+            for mod in sys.modules.values()
+            if hasattr(mod, '__file__') and hasattr(mod, '__name__')
+        }
+
+    return _get_module.index[context.filename]
+
+
+_get_module.index = {}
 
 
 def logger(context: Optional[inspect.FrameInfo] = None) -> logging.Logger:
@@ -11,7 +33,7 @@ def logger(context: Optional[inspect.FrameInfo] = None) -> logging.Logger:
     if context is None:
         context = inspect.getframeinfo(inspect.currentframe().f_back)
 
-    return logging.getLogger('bork.{}'.format(context.function))
+    return logging.getLogger('{}.{}'.format(_get_module(context), context.function))
 
 
 F = TypeVar('F', bound=Callable[..., Any])
