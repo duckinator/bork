d21c6492a739fcc0cf53fab68a967f02106eddac
Ellen Marie Dash <me@duckie.co>
2020-06-29 12:56:18 -0400

    Add more Pytest tests instead of relying on CI.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,7 +1,41 @@
+import subprocess
+import sys
+import tarfile
+import zipfile
+
 from click.testing import CliRunner  # noqa: I202
 import bork.cli
+
+
+def check_tgz(path):
+    assert tarfile.is_tarfile(path)
+    # tarfile.open() throws an exception if it's not a tarfile or we used
+    # the wrong mode.
+    tarfile.open(path, mode='r:gz')
+    return True
+
+
+def check_zipfile(path):
+    zf = zipfile.ZipFile(str(path))
+    bad_file = zf.testzip()
+    assert bad_file is None
+    return True
 
 
 def bork_cli(*args):
     runner = CliRunner()
     return runner.invoke(bork.cli.cli, args)
+
+
+def bork_check(*args):
+    result = bork_cli(*args)
+    assert result.exit_code == 0
+    return result
+
+
+def _check_call(cmd, **kwargs):
+    return subprocess.run(cmd, capture_output=True, text=True, check=True, **kwargs)
+
+
+def python_check(*args):
+    return _check_call([sys.executable, *args])
---
+++ tests/test_cmd_download.py
@@ -0,0 +1,27 @@
+from helpers import bork_check, python_check, check_zipfile, check_tgz
+
+
+def test_download():
+    # Download a pyz file from GitHub, saved to ./downloads, and check
+    # that it can be run with Python.
+    bork_check("download", "gh:duckinator/emanate", "v7.0.0")
+    python_check("downloads/emanate-7.0.0.pyz", "--help")
+
+    # Download a pyz file for a specific version from GitHub, saved to ./bin
+    bork_check("download", "gh:duckinator/emanate", "v7.0.0", "--directory", "bin/")
+    python_check("bin/emanate-7.0.0.pyz", "--help")
+
+    # Download a .tar.gz file from GitHub, saved to ./downloads
+    bork_check("download",
+               "gh:ppb/pursuedpybear", "v0.6.0",
+               "--files", "*.tar.gz",
+               "--directory", "downloads")
+    assert check_tgz("downloads/ppb-0.6.0.tar.gz")
+
+    # Download a .whl file from PyPi, and verify it's uncorrupted.
+    bork_check("download", "pypi:emanate", "v6.0.0", "--files", "*.whl")
+    assert check_zipfile("downloads/emanate-6.0.0-py3-none-any.whl")
+
+    # Download a .whl file from PyPi's test instance, and verify it's uncorrupted.
+    bork_check("download", "pypi-test:whaledo", "1.0.1", "--files", "*.whl")
+    assert check_zipfile("downloads/whaledo-1.0.1-py3-none-any.whl")
