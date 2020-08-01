48d2b1098c6e53969f02de2c2701f9d5329025f6
Ellen Marie Dash <me@duckie.co>
2020-08-01 17:35:58 -0400

    [tests/test_repos.py] Rename a function.

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -5,7 +5,7 @@ import pytest
 from helpers import bork_check, check_run
 
 
-def _repo_inner(tmpdir, repo):
+def _repo_test(tmpdir, repo):
     os.chdir(tmpdir)
     check_run(['git', 'clone', '--depth', '1', repo, str(Path(tmpdir, 'repo'))])
     os.chdir('repo')
@@ -17,22 +17,22 @@ def _repo_inner(tmpdir, repo):
 @pytest.mark.network
 @pytest.mark.slow
 def test_repo_ppb_vector(tmpdir):
-    _repo_inner(tmpdir, 'https://github.com/ppb/ppb-vector.git')
+    _repo_test(tmpdir, 'https://github.com/ppb/ppb-vector.git')
 
 
 @pytest.mark.network
 @pytest.mark.slow
 def test_repo_emanate(tmpdir):
-    _repo_inner(tmpdir, 'https://github.com/duckinator/emanate.git')
+    _repo_test(tmpdir, 'https://github.com/duckinator/emanate.git')
 
 
 @pytest.mark.network
 @pytest.mark.slow
 def test_repo_bork(tmpdir):
-    _repo_inner(tmpdir, 'https://github.com/duckinator/bork.git')
+    _repo_test(tmpdir, 'https://github.com/duckinator/bork.git')
 
 
 @pytest.mark.network
 @pytest.mark.slow
 def test_repo_ppb_mutant(tmpdir):
-    _repo_inner(tmpdir, 'https://github.com/astronouth7303/ppb-mutant.git')
+    _repo_test(tmpdir, 'https://github.com/astronouth7303/ppb-mutant.git')
