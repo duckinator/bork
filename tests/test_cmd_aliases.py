60cf0b8f96c287fd7801e52eac7f38f99dbf95fe
Ellen Marie Dash <me@duckie.co>
2020-08-01 17:30:43 -0400

    Add new aliases to tests/test_cmd_aliases.py.

--- tests/test_cmd_aliases.py
+++ tests/test_cmd_aliases.py
@@ -4,4 +4,4 @@ from helpers import bork_cli
 def test_cmd_aliases():
     result = bork_cli("aliases")
     assert result.exit_code == 0
-    assert result.output == 'lint\ntest\n'
+    assert result.output == 'lint\ntest\ntest-slow\ntest-all\n'
