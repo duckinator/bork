a9c54fc087a542160fa4fad58599b63fea8dd385
Ellen Marie Dash <me@duckie.co>
2023-07-10 15:15:21 -0400

    Fix test for `bork aliases`.

--- tests/test_cmd_aliases.py
+++ tests/test_cmd_aliases.py
@@ -4,4 +4,4 @@ from helpers import bork_cli
 def test_cmd_aliases():
     result = bork_cli("aliases")
     assert result.exit_code == 0
-    assert result.output == 'lint\ntest\ntest-fast\ntest-slow\n'
+    assert result.output == 'lint\ntest\ntest-fast\ntest-slow\ndocs\n'
