dec2b06cb7692f7d3ddd00718e201ce5ce54cff6
nicoo <nicoo@mur.at>
2023-11-17 13:21:35 +0000

    tests: Add minimal package to build without network

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,19 +6,27 @@ import pytest
 from helpers import check_run
 
 
-def _url_name(url):
-    return url.rsplit('/', 1)[1].removesuffix('.git')
+def _src_name(src):
+    if isinstance(src, Path):
+        return src.name
 
-@pytest.mark.network
-@pytest.fixture(scope="session", ids=_url_name, params=(
+    return src.rsplit('/', 1)[1].removesuffix('.git')
+
+
+@pytest.fixture(scope="session", ids=_src_name, params=(
+    Path(__file__).parent / 'fixtures' / 'minimal-package',
+    ) + tuple(pytest.param(url, marks=pytest.mark.network) for url in (
     "https://github.com/astronouth7303/ppb-mutant.git",
     "https://github.com/duckinator/bork.git",
     "https://github.com/duckinator/emanate.git",
     "https://github.com/ppb/ppb-vector.git",
-))
+)))
 def project_src(request, tmp_path_factory):
+    if isinstance(request.param, Path):
+        return request.param
+
     url = request.param
-    dest = tmp_path_factory.mktemp(_url_name(url))
+    dest = tmp_path_factory.mktemp(_src_name(url))
     check_run(['git', 'clone', '--depth', '1', url, dest])
     return dest
 
