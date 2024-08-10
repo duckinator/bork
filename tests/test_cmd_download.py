a3057f94cc85345b683a29e95e342fa9df42c942
nicoo <nicoo@mur.at>
2024-08-10 22:16:58 +0000

    tests/cmd_download: split up in smaller tests

--- tests/test_cmd_download.py
+++ tests/test_cmd_download.py
@@ -3,7 +3,7 @@ from helpers import chdir, bork_check, python_check, check_zipfile, check_tgz
 
 
 @pytest.mark.network
-def test_download(tmpdir):
+def test_download_pyz(tmpdir):
     with chdir(tmpdir):
         # Download a pyz file from GitHub, saved to ./downloads, and check
         # that it can be run with Python.
@@ -14,6 +14,9 @@ def test_download(tmpdir):
         bork_check("download", "gh:duckinator/emanate", "v7.0.0", "--directory", "bin/")
         python_check("bin/emanate-7.0.0.pyz", "--help")
 
+@pytest.mark.network
+def test_download_tgz(tmpdir):
+    with chdir(tmpdir):
         # Download a .tar.gz file from GitHub, saved to ./downloads
         bork_check("download",
                    "gh:ppb/pursuedpybear", "v0.6.0",
@@ -21,10 +24,13 @@ def test_download(tmpdir):
                    "--directory", "downloads")
         assert check_tgz("downloads/ppb-0.6.0.tar.gz")
 
+@pytest.mark.network
+@pytest.mark.parametrize("repo,pkg,version", (
+    ("pypi", "emanate", "6.0.0"),
+    ("pypi-test", "whaledo", "1.0.1")
+))
+def test_download_whl(tmpdir, repo, pkg, version):
+    with chdir(tmpdir):
         # Download a .whl file from PyPi, and verify it's uncorrupted.
-        bork_check("download", "pypi:emanate", "v6.0.0", "--files", "*.whl")
-        assert check_zipfile("downloads/emanate-6.0.0-py3-none-any.whl")
-
-        # Download a .whl file from PyPi's test instance, and verify it's uncorrupted.
-        bork_check("download", "pypi-test:whaledo", "1.0.1", "--files", "*.whl")
-        assert check_zipfile("downloads/whaledo-1.0.1-py3-none-any.whl")
+        bork_check("download", f"{repo}:{pkg}", version, "--files", "*.whl")
+        assert check_zipfile(f"downloads/{pkg}-{version}-py3-none-any.whl")
