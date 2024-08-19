2f9e136d42e281bf5e426b9712d243240dd056f1
nicoo <nicoo@mur.at>
2024-08-19 22:25:16 +0000

    tests/repos: check that zipapps are built and work

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -1,6 +1,18 @@
-from helpers import bork_check
+from bork.config import Config
+from helpers import bork_check, python_check
 
 def test_repo(project):
     bork_check('clean')
     bork_check('build')
     bork_check('release', '--dry-run')
+
+    if not Config.from_project(project).bork.zipapp.enabled:
+        return
+
+    match tuple((project / "dist").glob("*.pyz")):
+        case []:
+            assert False, "No zipapp was produced"
+        case [zipapp]:
+            python_check(zipapp, "--version")
+        case _:
+            assert False, "Multiple zipapps were produced?  O_o"
